6ca7734a34686eb8edbdbee1462af31bc3a089bf
Afzal Hossain <uttam028@users.noreply.github.com>
2024-12-01 22:25:02 -0500

    Revert "Revert "DATAINT-1917: Extend membership endpoint to take manual ID input""

--- PluginHubspot/DataContracts/CustomWriteFormData.cs
+++ PluginHubspot/DataContracts/CustomWriteFormData.cs
@@ -1,33 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using Newtonsoft.Json;
-
-namespace PluginHubspot.DataContracts
+﻿namespace PluginHubspot.DataContracts
 {
     public class CustomWriteFormData
     {
-        public Hubspot Hubspot { get; set; }
-    }
-
-    public class Hubspot
-    {
         public string Endpoint { get; set; }
-        public EndpointSettings EndpointSettings { get; set; }
-    }
-
-    public class EndpointSettings
-    {
         public MembershipsSettings? MembershipsSettings { get; set; }
     }
 
     public class MembershipsSettings
     {
-        public string IlsId { get; set; }
+        public string? IlsId { get; set; }
+        public string? ManualIlsId { get; set; }
         public string ParseIlsId()
         {
-            return IlsId[(IlsId.LastIndexOf('(') + 1)..IlsId.LastIndexOf(')')];
+            return IlsId![(IlsId!.LastIndexOf('(') + 1)..IlsId.LastIndexOf(')')];
         }
     }
 }
